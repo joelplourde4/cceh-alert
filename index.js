b377dee8bfa9d5f8db5a48b1718b9af73ef0d52d
Joel Plourde <100144761+joelplourde4@users.noreply.github.com>
2025-09-13 11:25:53 -0400

    Add Express app listening on PORT (#5)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const scrapeData = require('./scripts/scrape');
 const runAnalysis = require('./scripts/analyze');
 const { validateConfiguration } = require('./lib/alerts');
+const express = require('express');
 
 const INTERVAL = 15 * 60 * 1000; // 15 minutes
 
@@ -25,3 +26,18 @@ scrapeData().then(() => {
 
 // Then set an interval to run the scripts periodically
 setInterval(runScripts, INTERVAL);
+
+const app = express();
+
+// Simple route
+app.get("/", (req, res) => {
+  res.send("Hello from Render!");
+});
+
+// Render provides the port as an environment variable
+const PORT = process.env.PORT || 8080;
+
+// Bind to 0.0.0.0
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
